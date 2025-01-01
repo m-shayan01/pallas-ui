628c03d40f6194eb3019f64e1b086a42ccd6b513
Kashif <kashif.ali@carbonteq.com>
2025-01-01 15:06:51 +0500

    feat: add align variants

--- components/src/stories/Popover.stories.tsx
+++ components/src/stories/Popover.stories.tsx
@@ -3,6 +3,7 @@ import { Stack } from '@styled-system/jsx'
 import { X } from 'lucide-react'
 import { Button } from '~/ui/button'
 import Popover from '~/ui/popover'
+import { Heading } from '~/ui/typography'
 
 const meta: Meta<typeof Popover.Root> = {
   component: Popover.Root,
@@ -26,7 +27,7 @@ export const Default = () => (
   </Popover.Root>
 )
 
-const popovers = [
+const popoverVariantsWithSides = [
   {
     triggerText: 'Top',
     position: 'top',
@@ -53,35 +54,76 @@ const popovers = [
   },
 ]
 
+const popoverVariantsWithAligns = [
+  {
+    triggerText: 'Start',
+    align: 'start',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+  },
+  {
+    triggerText: 'Center',
+    align: 'center',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+  },
+  {
+    triggerText: 'End',
+    align: 'end',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+  },
+]
+
+type Side = 'top' | 'bottom' | 'left' | 'right'
+type Align = 'start' | 'center' | 'end'
+
 export const Variants = () => (
-  <Stack align="center">
-    <Stack gap={4} css={{ maxWidth: 200 }}>
-      {popovers.map((item) => (
+  <Stack>
+    <Heading level={4}>Side</Heading>
+    <Stack direction="row" gap={4} css={{ maxWidth: 200 }}>
+      {popoverVariantsWithSides.map((item) => (
         <Popover.Root key={item.triggerText}>
           <Popover.Trigger asChild>
             <Button>{item.triggerText}</Button>
           </Popover.Trigger>
           <Popover.Portal>
-            <Popover.Content side={item.position as unknown as 'left'} sideOffset={5}>
+            <Popover.Content align="start" side={item.position as Side} sideOffset={5}>
               {item.content}
             </Popover.Content>
           </Popover.Portal>
         </Popover.Root>
       ))}
-      <Popover.Root>
-        <Popover.Trigger asChild>
-          <Button>With Close Icon</Button>
-        </Popover.Trigger>
-        <Popover.Portal>
-          <Popover.Content sideOffset={5}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-            <Popover.Close css={{ position: 'absolute', top: 5, right: 5 }}>
-              <X width={16} height={16} />
-            </Popover.Close>
-          </Popover.Content>
-        </Popover.Portal>
-      </Popover.Root>
     </Stack>
+    <Heading level={4}>Align</Heading>
+    <Stack direction="row" gap={4} css={{ maxWidth: 200 }}>
+      {popoverVariantsWithAligns.map((item) => (
+        <Popover.Root key={item.triggerText}>
+          <Popover.Trigger asChild>
+            <Button>{item.triggerText}</Button>
+          </Popover.Trigger>
+          <Popover.Portal>
+            <Popover.Content align={item.align as Align} sideOffset={5}>
+              {item.content}
+            </Popover.Content>
+          </Popover.Portal>
+        </Popover.Root>
+      ))}
+    </Stack>
+    <Heading level={4}>Close</Heading>
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <Button>Open Popover</Button>
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content sideOffset={5}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Quisquam, quos.
+          <Popover.Close css={{ position: 'absolute', top: 5, right: 5 }}>
+            <X width={16} height={16} />
+          </Popover.Close>
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   </Stack>
 )
